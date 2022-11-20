97280ad4a811e208a306a23921815d3e3661b48c
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-11-20 19:39:28 +0600

    jwt token setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors")
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken") 
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -81,6 +82,20 @@ async function run(){
             const result = await bookingsCollection.insertOne(booking)
             res.send(result)
         });
+
+// JWT token setup
+
+        app.get("/jwt",async(req,res)=>{
+            const email = req.query.email;
+            const query = { email:email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email},process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+                return res.send({accessToken: token});
+            }
+            console.log(user);
+            res.status(403).send({accessToken: ''})
+        })
 
         app.post("/users",async(req,res)=>{
             const user = req.body;
